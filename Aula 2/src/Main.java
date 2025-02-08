dc5c86941d69c587e63c8fa822d9646a0885c88c
IgorToledoOFC <igorsilvatoledoofc@gmail.com>
2025-02-07 22:43:46 -0300

    Exercicio 1 de Hello World

---
+++ Aula 2/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Java World");
+
+        int x = 10;
+        int y = 20;
+
+        int z = x + y;
+
+        System.out.print("O resultado de Z é: "+ z);
+    }
+}
